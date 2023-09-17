d23d1600d5bb388111cf5261d6a92cb74ff416d6
Jeannuel García <jjeanlink@gmail.com>
2023-09-17 18:46:03 -0500

    styles: fix some styling

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+import { HomeLayoutComponent } from './layout/home-layout/home-layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: PokemonListComponent,
-  },
-  {
-    path: 'detail/:id',
-    component: PokemonDetailComponent,
+    component: HomeLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: PokemonListComponent,
+      },
+      {
+        path: 'detail/:id',
+        component: PokemonDetailComponent,
+      },
+    ],
   },
 ];
 
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,9 +5,14 @@ import { SharedModule } from './../../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+import { HomeLayoutComponent } from './layout/home-layout/home-layout.component';
 
 @NgModule({
-  declarations: [PokemonListComponent, PokemonDetailComponent],
+  declarations: [
+    PokemonListComponent,
+    PokemonDetailComponent,
+    HomeLayoutComponent,
+  ],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
